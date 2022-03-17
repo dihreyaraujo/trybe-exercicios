518c9d7fb654e01ba82e9d0250ea87283a711888
Dihrey Araujo <99283526+dihreyaraujo@users.noreply.github.com>
2022-03-17 12:36:03 -0300

    exercício 1 e 2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,59 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+
+let monthDaysList = document.querySelector('#days'); 
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function monthDays() {
+    let dayList = 0;
+    for(let index = 0; index < dezDaysList.length; index += 1) {
+        dayList = dezDaysList[index];
+        let elemento = document.createElement('li');
+        elemento.className = 'day'
+        elemento.innerText = dayList;
+        if(dayList == 24 || dayList == 25 || dayList == 31){
+            elemento.className = 'day holiday';
+        }
+        if(dayList == 4 || dayList == 11 || dayList == 18) {
+            elemento.className = 'day friday';
+        }
+        if(dayList == 25) {
+            elemento.className = 'day holiday friday'
+        }
+        
+        monthDaysList.appendChild(elemento);
+
+    }
+}
+
+monthDays();
+
+//
+
+
+let containerButton = document.querySelector('.buttons-container');
+
+function holiday(string) {
+    let button = document.createElement('button');
+    button.id = 'btn-holiday'
+    button.innerText = string;
+    containerButton.appendChild(button);
+
+}
+
+holiday('feriados')
