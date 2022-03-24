683f58f13500bc239d711874cfc24f13fabfa911
Dihrey Araujo <99283526+dihreyaraujo@users.noreply.github.com>
2022-03-24 16:06:45 -0300

    Exercicio b6 dia 1 html e css forms

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,7 @@
+let btnSubmit = document.getElementById('enviar');
+
+btnSubmit.addEventListener('click', function(event){
+    event.preventDefault();
+})
+
+let btnApaga = document.getElementById('apaga');
